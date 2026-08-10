9821a15f36421662a4a8c30ef466466efff4da7a
Raskinn <razkyn@outlook.com>
2026-08-10 13:10:59 +0530

    Added changes to backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
     bucket        = "dev-tf-state-bucket-ap-south-2"  # Match your S3 bucket name
+    key           = "dev/terraform.tfstate"           # Path to state file in S3
     region        = "ap-south-2"
     use_lockfile  = true                              # Enables native S3 state locking
     encrypt       = true
